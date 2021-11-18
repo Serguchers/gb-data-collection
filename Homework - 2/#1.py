2454700eff1a9eb72bd37f27a514ad3ac85e81ec
Sergei <serguchers@gmail.com>
2021-11-18 23:13:16 +0300

    Lesson 2. Refactoring

--- Homework - 2/#1.py
+++ Homework - 2/#1.py
@@ -1,45 +1,43 @@
+import json
+from pprint import pprint, saferepr
+
 import requests
 from bs4 import BeautifulSoup
-from pprint import pprint, saferepr
-import json
-
 
 params = {
-    'fromSearchLine': 'true',
-    'text': 'python', 
-    'search_field': ['description', 'company_name', 'name'],
-    'page': 0
+    "fromSearchLine": "true",
+    "text": "python",
+    "search_field": ["description", "company_name", "name"],
+    "page": 0,
 }
-headers = {
-    'User-Agent': 'Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14'
-}
-url = 'https://kaliningrad.hh.ru'
+headers = {"User-Agent": "Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14"}
+url = "https://kaliningrad.hh.ru"
 parsed_data = []
 
 
-def currency_handler(value:list):
-    cur_dict = {'min': None, 'max': None, 'cur': value[-1]}
+def currency_handler(value: list):
+    cur_dict = {"min": None, "max": None, "cur": value[-1]}
 
-    if len(value) == 7 and 'до' in value[0]:
-        curr = int(''.join(el for el in value[2] if el.isdecimal()))
-        cur_dict['max'] = curr
+    if len(value) == 7 and "до" in value[0]:
+        curr = int("".join(el for el in value[2] if el.isdecimal()))
+        cur_dict["max"] = curr
 
         return cur_dict
-    
-    if len(value) == 7 and 'от' in value[0]:
-        curr = int(''.join(el for el in value[2] if el.isdecimal()))
-        cur_dict['min'] = curr
+
+    if len(value) == 7 and "от" in value[0]:
+        curr = int("".join(el for el in value[2] if el.isdecimal()))
+        cur_dict["min"] = curr
 
         return cur_dict
 
     if len(value) == 3:
-        min = value[0].replace(' ', '').split('–')[0]
-        curr = int(''.join(el for el in min if el.isdecimal()))
-        cur_dict['min'] = curr
+        min = value[0].replace(" ", "").split("–")[0]
+        curr = int("".join(el for el in min if el.isdecimal()))
+        cur_dict["min"] = curr
 
-        max = value[0].replace(' ', '').split('–')[1]
-        curr = int(''.join(el for el in max if el.isdecimal()))
-        cur_dict['max'] = curr
+        max = value[0].replace(" ", "").split("–")[1]
+        curr = int("".join(el for el in max if el.isdecimal()))
+        cur_dict["max"] = curr
 
         return cur_dict
 
@@ -47,34 +45,34 @@ def currency_handler(value:list):
 def vacancy_handler(vacancy):
     result = {}
 
-    info = vacancy.find('a', {'class': 'bloko-link'})
+    info = vacancy.find("a", {"class": "bloko-link"})
     name = info.getText()
     try:
-        salary = vacancy.find('span', {'data-qa': 'vacancy-serp__vacancy-compensation'}).contents
+        salary = vacancy.find("span", {"data-qa": "vacancy-serp__vacancy-compensation"}).contents
         salary = currency_handler(salary)
     except AttributeError:
         salary = None
-    link = info['href']
+    link = info["href"]
     main_site = response.url
 
-    result['name'] = name
-    result['salary'] = salary
-    result['link'] = link
-    result['main_site'] = main_site
-    
+    result["name"] = name
+    result["salary"] = salary
+    result["link"] = link
+    result["main_site"] = main_site
+
     return result
 
 
 for page in range(int(input())):
-    response = requests.get(url + '/search/vacancy', params=params, headers=headers)
-    dom = BeautifulSoup(response.text, 'html.parser')
-    vacancies_list = dom.find_all('div', {'class': 'vacancy-serp-item'})
-    
+    response = requests.get(url + "/search/vacancy", params=params, headers=headers)
+    dom = BeautifulSoup(response.text, "html.parser")
+    vacancies_list = dom.find_all("div", {"class": "vacancy-serp-item"})
+
     for vacancy in vacancies_list:
         parsed_data.append(vacancy_handler(vacancy))
-    
-    params['page'] += 1
+
+    params["page"] += 1
 
 
-with open('Homework - 2\hh_parsed.json', 'w') as f:
+with open("Homework - 2\hh_parsed.json", "w") as f:
     json.dump(parsed_data, f)
--- Homework - 2/#2.py
+++ Homework - 2/#2.py
@@ -1,54 +1,50 @@
+import json
+from pprint import pprint, saferepr
+
 import requests
 from bs4 import BeautifulSoup
-from pprint import pprint, saferepr
-import json
 
-
-params = {
-    'keyword': 'чипсы',
-    'page': 1
-}
-headers = {
-    'User-Agent': 'Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14'
-}
-url = 'https://roscontrol.com'
+params = {"keyword": "чипсы", "page": 1}
+headers = {"User-Agent": "Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14"}
+url = "https://roscontrol.com"
 parsed_data = []
 
 
-def parametres_parser(parametres:list):
+def parametres_parser(parametres: list):
     product_metrics = {}
-    product_metrics['Безопасность'] = int(parametres[0]['data-width'])
-    product_metrics['Пищевая ценность'] = int(parametres[1]['data-width'])
-    product_metrics['Натуральность'] = int(parametres[2]['data-width'])
-    product_metrics['Качество'] = int(parametres[3]['data-width'])
+    product_metrics["Безопасность"] = int(parametres[0]["data-width"])
+    product_metrics["Пищевая ценность"] = int(parametres[1]["data-width"])
+    product_metrics["Натуральность"] = int(parametres[2]["data-width"])
+    product_metrics["Качество"] = int(parametres[3]["data-width"])
 
     return product_metrics
 
+
 for page in range(int(input())):
-    response = requests.get(url + '/testlab/search', params=params, headers=headers)
-    dom = BeautifulSoup(response.text, 'html.parser')
-    products_list = dom.find_all('div', {'class': 'wrap-product-catalog__item'})
-    
+    response = requests.get(url + "/testlab/search", params=params, headers=headers)
+    dom = BeautifulSoup(response.text, "html.parser")
+    products_list = dom.find_all("div", {"class": "wrap-product-catalog__item"})
+
     for product in products_list:
         product_info = {}
 
-        info = product.find('div', {'class': 'product__item-link'})
+        info = product.find("div", {"class": "product__item-link"})
         name = info.getText()
-        parametres = parametres_parser(product.find_all('i'))
+        parametres = parametres_parser(product.find_all("i"))
         try:
-            rating = int(product.find('div', {'class': 'rate'}).getText())
+            rating = int(product.find("div", {"class": "rate"}).getText())
         except:
             rating = None
         website = response.url
 
-        product_info['name'] = name
-        product_info['parametres'] = parametres
-        product_info['rating'] = rating
-        product_info['website'] = website
+        product_info["name"] = name
+        product_info["parametres"] = parametres
+        product_info["rating"] = rating
+        product_info["website"] = website
 
         parsed_data.append(product_info)
 
-    params['page'] += 1
+    params["page"] += 1
 
-with open('Homework - 2\\roscontrol_product.json', 'w') as f:
+with open("Homework - 2\\roscontrol_product.json", "w") as f:
     json.dump(parsed_data, f)
