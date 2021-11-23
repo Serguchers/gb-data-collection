61d53aaec21cd53c5d1834a9675dc61fc88b6353
Sergei <serguchers@gmail.com>
2021-11-23 20:43:46 +0300

    Lesson 3. Currency additions

--- Homework - 3/#2.py
+++ Homework - 3/#2.py
@@ -10,17 +10,20 @@ db = client["hh_vacancies_python"]
 vacancies = db.vacancies
 
 
-def salary_filter(wage: int):
+def salary_filter(wage: int, cur_name:str):
+    if cur_name == 'RUB':
+        cur_name = 'руб.'
     for doc in vacancies.find({}):
         try:
-            if doc["salary"]["min"] >= wage or doc["salary"]["max"] >= wage:
-                pprint(doc)
+            if doc['salary']['cur'] == cur_name:
+                if doc["salary"]["min"] >= wage or doc["salary"]["max"] >= wage:
+                    pprint(doc)
         except:
             continue
 
 
 if __name__ == "__main__":
-    salary_filter(350000)
+    salary_filter(3000, 'USD')
 
 # Вероятно в этом задании подразумевалось использование операторов непосредственно в запросе mongo,
 # но я не нашел простых путей для написания вложенных запросов, поэтому применил обычное сравнение.
